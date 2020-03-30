c811d66bf7d404099df4449bab9d9a9755ee65bc
sunyue89 <sunyue89@gmail.com>
2020-03-29 23:55:35 -0400

    Update strongly_connected.cpp

--- graph/hw2/strongly_connected.cpp
+++ graph/hw2/strongly_connected.cpp
@@ -7,17 +7,13 @@ using std::pair;
 
 void dfs(vector<vector<int> > &adj, vector<int> &used, vector<int> &order, int x) {
   //write your code here
-	used[x] = true;
-
-
-	for(size_t i=0;i<adj[x].size();++i){
+  used[x] = true;
+  for(size_t i=0;i<adj[x].size();++i){
 		if(!used[adj[x][i]])
 			dfs(adj,used,order,adj[x][i]);
-	}
-
+		}
   order.insert(order.begin(),x);
-
-	return;
+  return;
   
 }     
 
@@ -25,7 +21,7 @@ vector<int> toposort(vector<vector<int> > adj) {
   vector<int> used(adj.size(), 0);
   vector<int> order;
   //write your code here
-	for(size_t i =0; i<adj.size(); ++i){
+  for(size_t i =0; i<adj.size(); ++i){
 		if(!used[i]){
 			dfs(adj,used,order,i); 
 		}
@@ -34,12 +30,12 @@ vector<int> toposort(vector<vector<int> > adj) {
 }
 
 vector<vector<int>> rev(vector<vector<int>> adj){
-	vector<vector<int>> trav(adj.size(),vector<int>());
-	for (size_t i=0; i<adj.size(); ++i){
-		for (size_t j=0; j<adj[i].size();++j)
-			trav[adj[i][j]].push_back(i);
-	} 
-	return trav;
+  vector<vector<int>> trav(adj.size(),vector<int>());
+  for (size_t i=0; i<adj.size(); ++i){
+	for (size_t j=0; j<adj[i].size();++j)
+		trav[adj[i][j]].push_back(i);
+  } 
+  return trav;
 }
 
 int number_of_strongly_connected_components(vector<vector<int> > adj) {
